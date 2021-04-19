154b6eb95ca9183acb03807731e65d5ed7db0487
Christopher Mabry <chrismabryprof9@gmail.com>
2021-04-19 11:26:48 -0400

    First commit

--- cityApp/ViewController.swift
+++ cityApp/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Christopher Mabry on 4/19/21.
 //
+//  Planning Doc at https://www.notion.so/City-Activities-Map-ec8002b766df4ac0b113bdba3ffe53f3
 
 import UIKit
 
@@ -11,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
