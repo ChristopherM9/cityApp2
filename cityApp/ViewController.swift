aea7f138c49cc987aaff998928a1b79bae7f0df6
Christopher Mabry <chrismabryprof9@gmail.com>
2021-04-20 11:47:40 -0400

    Added Map Kit and some code

--- cityApp/ViewController.swift
+++ cityApp/ViewController.swift
@@ -7,13 +7,28 @@
 //  Planning Doc at https://www.notion.so/City-Activities-Map-ec8002b766df4ac0b113bdba3ffe53f3
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
 
 }
 
+}
